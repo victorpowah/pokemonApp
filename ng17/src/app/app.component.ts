b69868b3a137f3ded2959bc1e5ef0a40fdc6d916
victorpowah <g3victorrodriguez@gmail.com>
2024-02-07 10:35:06 +0100

    toolbarComponent

--- ng17/src/app/app.component.ts
+++ ng17/src/app/app.component.ts
@@ -3,24 +3,21 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { PrimeNGConfig } from 'primeng/api';
 import { TranslateService } from '@ngx-translate/core';
-import { ButtonModule } from 'primeng/button';
-import { RippleModule } from 'primeng/ripple';
-import { ThemeService } from './services/theme-service.service';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ButtonModule, RippleModule],
+  imports: [CommonModule, RouterOutlet, ToolbarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   title = 'ng17';
-  themeSelection: boolean = false;
+
   constructor(
     private primengConfig: PrimeNGConfig,
-    private translateService: TranslateService,
-    private readonly themeService: ThemeService
+    private translateService: TranslateService
   ) {}
 
   ngOnInit(): void {
@@ -40,10 +37,5 @@ export class AppComponent implements OnInit {
     this.translateService
       .get('primeng')
       .subscribe((res: any) => this.primengConfig.setTranslation(res));
-  }
-
-  changeTheme(): void {
-    this.themeSelection = !this.themeSelection;
-    this.themeService.changeTheme(this.themeSelection ? 'dark' : 'light');
   }
 }
