eba94c6f9fa6e9a1c8f57b5baab908557bc1e55a
victorpowah <g3victorrodriguez@gmail.com>
2024-02-09 01:30:23 +0100

    card styles

--- src/app/components/pokemon-card/pokemon-card.component.spec.ts
+++ src/app/components/pokemon-card/pokemon-card.component.spec.ts
@@ -1,23 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { PokemonCardComponent } from './pokemon-card.component';
+import { PokemonCardComponent } from './pokemon-card.component'
 
 describe('PokemonCardComponent', () => {
-  let component: PokemonCardComponent;
-  let fixture: ComponentFixture<PokemonCardComponent>;
+  let component: PokemonCardComponent
+  let fixture: ComponentFixture<PokemonCardComponent>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PokemonCardComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(PokemonCardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+      imports: [PokemonCardComponent],
+    }).compileComponents()
+
+    fixture = TestBed.createComponent(PokemonCardComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
