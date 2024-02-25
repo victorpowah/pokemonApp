1aad55e4c55d9f27a480162314f7fc750e2f108f
Alvaro Cebrian 2A <113922067+CebrianAlvaro9@users.noreply.github.com>
2024-02-25 19:44:40 +0100

    translations completed

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core'
 import { ButtonModule } from 'primeng/button'
 import { TranslateModule } from '@ngx-translate/core'
+import { RouterLink } from '@angular/router'
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ButtonModule, TranslateModule],
+  imports: [ButtonModule, TranslateModule, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
