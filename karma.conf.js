72bfaf1eac6ba200a5d2770b10db909d14c01c5e
victorpowah <g3victorrodriguez@gmail.com>
2024-03-13 19:42:04 +0100

    changes

--- karma.conf.js
+++ karma.conf.js
@@ -34,7 +34,19 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromiumNoSandbox', 'Chrome'],
+    customLaunchers: {
+      ChromiumNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+          '--headless',
+          '--disable-gpu',
+          '--disable-translate',
+          '--disable-extensions',
+        ],
+      },
+    },
     singleRun: false,
     restartOnFileChange: true,
   })
