eafc7c2ad797f1ebd85f9c32fa7d72cf2315ebdb
Alvaro Cebrian 2A <113922067+CebrianAlvaro9@users.noreply.github.com>
2024-03-13 14:33:29 +0100

    test

--- src/app/components/items/items.component.spec.ts
+++ src/app/components/items/items.component.spec.ts
@@ -1,36 +1,56 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { of } from 'rxjs'
 
 import { ItemsComponent } from './items.component'
-import { TranslateModule } from '@ngx-translate/core'
-import { RouterModule } from '@angular/router'
 import { PokeApiService } from '../../services/poke-api.service'
+import { DestroyService } from '../../services/destroy.service'
+import { PokeApiResponse } from '../../models/pokeApi-response.model'
+import { PaginatorState } from 'primeng/paginator'
+import { TranslateModule } from '@ngx-translate/core'
 
 describe('ItemsComponent', () => {
   let component: ItemsComponent
   let fixture: ComponentFixture<ItemsComponent>
   let pokeApiService: jasmine.SpyObj<PokeApiService>
-  beforeEach(async () => {
-    const pokeApiServiceSpy = jasmine.createSpyObj('PokeApiService', [
-      'getAbility',
-    ])
+  let destroyService: DestroyService
 
-    await TestBed.configureTestingModule({
-      imports: [
-        ItemsComponent,
-        TranslateModule.forRoot(),
-        RouterModule.forRoot([]),
+  beforeEach(() => {
+    pokeApiService = jasmine.createSpyObj('PokeApiService', ['getItems'])
+    destroyService = new DestroyService()
+
+    TestBed.configureTestingModule({
+      imports: [ItemsComponent, TranslateModule.forRoot()],
+      providers: [
+        { provide: PokeApiService, useValue: pokeApiService },
+        { provide: DestroyService, useValue: destroyService },
       ],
-      providers: [{ provide: PokeApiService, useValue: pokeApiServiceSpy }],
-    }).compileComponents()
-    pokeApiService = TestBed.inject(
-      PokeApiService
-    ) as jasmine.SpyObj<PokeApiService>
+    })
+
     fixture = TestBed.createComponent(ItemsComponent)
     component = fixture.componentInstance
-    fixture.detectChanges()
   })
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('should fetch items on ngOnInit', () => {
+    const mockItemsResponse: PokeApiResponse = {
+      count: 0,
+      next: '',
+      previous: '',
+      results: [],
+    }
+    pokeApiService.getItems.and.returnValue(of(mockItemsResponse))
+
+    component.ngOnInit()
+
+    expect(component.items).toEqual(mockItemsResponse)
+  })
+
+  // Add more tests as needed
+  it('should change row', () => {
+    component.changeRow()
+    expect(component.rows).toBe(20)
   })
 })
